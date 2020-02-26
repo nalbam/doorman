9e322900e90079b1dff679a13741d6c6caea1273
nalbam <me@nalbam.com>
2020-02-26 17:55:41 +0900

    doorman-users

--- amplify/backend/function/users/src/app.js
+++ amplify/backend/function/users/src/app.js
@@ -17,7 +17,7 @@ AWS.config.update({ region: process.env.TABLE_REGION });
 
 const dynamodb = new AWS.DynamoDB.DocumentClient();
 
-let tableName = "deeplens-doorman";
+let tableName = "doorman-users";
 if (process.env.ENV && process.env.ENV !== "NONE") {
   tableName = tableName + '-' + process.env.ENV;
 }
