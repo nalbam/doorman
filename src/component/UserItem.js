f3b8a728f5eefea9e0716cdf92910055229d10f8
nalbam <me@nalbam.com>
2020-02-26 19:06:06 +0900

    href

--- src/component/UserItem.js
+++ src/component/UserItem.js
@@ -2,10 +2,12 @@ import React, { Component, Fragment } from 'react';
 
 class App extends Component {
   render() {
+    let href = `/users/${this.props.item.user_id}`;
+
     return (
       <Fragment>
         <div className='person-item'>
-          <div><img src={this.props.item.image_url} alt={this.props.item.user_name} className='person-photo' /></div>
+          <div><a href={href}><img src={this.props.item.image_url} alt={this.props.item.user_name} className='person-photo' /></a></div>
           <div>{this.props.item.real_name}</div>
         </div>
       </Fragment>
