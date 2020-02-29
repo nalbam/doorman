d21c3fa3e627f08ed6d3e7a3742b849985dc45bd
nalbam <me@nalbam.com>
2020-02-29 18:39:08 +0900

    app.listen

--- amplify/backend/function/users/src/app.js
+++ amplify/backend/function/users/src/app.js
@@ -165,6 +165,11 @@ app.put(path, function (req, res) {
   });
 });
 
+
+app.listen(3000, function () {
+  console.log('App started')
+});
+
 // Export the app object. When executing the application local this does nothing. However,
 // to port it to AWS Lambda we will create a wrapper around that will load the app from
 // this file
